436e0ef97902d2295e018daae7d476f7d892764d
NguyenVanPhuong <nguyenvanphuong8522@gmail.com>
2023-12-24 00:18:08 +0700

    update interaction

---
+++ VRTK-Unity 2019.4.28/Assets/Scritps/OnOFF.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOFF : MonoBehaviour
+{
+    [SerializeField]
+    private MeshRenderer mesh;
+    public void OnOff()
+    {
+        if(mesh.enabled)
+        {
+            mesh.enabled = false;
+        }
+        else
+        {
+            mesh.enabled = true;
+        }
+    }
+}
---
+++ VRTK-Unity 2019.4.28/Assets/Scritps/demoRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demoRotate : MonoBehaviour
+{
+    private Rigidbody rb;
+    [Range(0f, 100f)]
+    public float force;
+    private bool turnOn = false;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezePosition;
+    }
+    public void TurnOn()
+    {
+        turnOn = !turnOn;
+    }
+    private void FixedUpdate()
+    {
+        if (turnOn)
+        {
+            rb.AddTorque(Vector3.up * force);
+        }
+    }
+}
